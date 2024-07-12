826a96b0ba60ef93f273f17c18de2e85065651c7
srisailavanya <srisailavanya@gmail.com>
2024-07-12 09:39:16 +0530

    done

---
+++ Even.java
@@ -0,0 +1,15 @@
+class Even 
+{
+public static void main(string args[])
+
+int m1=26;
+int m2=45;
+if(n%2==0)
+{
+ System.out.println(this number is even)
+ }
+else
+{
+System.out.println(this number is not even)
+}
+}
---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+class Subtraction
+{
+public Satic void main (String args[])
+
+int m1=20;
+int m2=35;
+int add=m1+m2;
+System.out.println(the given number is Subtraction)
+}
